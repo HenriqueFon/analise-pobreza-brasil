cbf82f453c661ec6e2e96bc5088d4d194a6bffba
marcelochaves <mcmendes@uolinc.com>
2023-10-25 01:58:01 -0300

    feat: calculate poor and vulnarable percentage

--- estudo-leitura-dos-dados/app.py
+++ estudo-leitura-dos-dados/app.py
@@ -1,23 +1,5 @@
 import pandas as pd
 
-
-def createXlsx(data):
-    dicionary = [{'tag': tag, 'descricao': descricao}
-                 for tag, descricao in zip(dicionaryTags, dicionaryTypes)]
-
-    df_dict = pd.DataFrame(data)
-    df_dicionary = pd.DataFrame(dicionary)
-
-    # Salve o DataFrame do dicionário em uma planilha específica dentro do arquivo CSV
-    with pd.ExcelWriter('analise_pobreza_brasil.xlsx', engine='openpyxl') as writer:
-        df_dict.to_excel(writer, sheet_name='Dados', index=False)
-        df_dicionary.to_excel(writer, sheet_name='Dicionario', index=False)
-
-    return
-
-
-csv = pd.read_csv("MunicipioBrasil_20230102.csv")
-
 dicionaryTags = [
     'cod_regiao',
     'qtd_pes',
@@ -34,4 +16,65 @@ dicionaryTypes = [
     'Número de pessoas pobres ou vulneráveis'
 ]
 
-createXlsx(csv)
+regionTags = [1, 2, 3, 4, 5]
+
+regionTypes = [
+    'Norte',
+    'Nordeste',
+    'Sudeste',
+    'Sul',
+    'Centro-Oeste'
+]
+
+
+# Cria o xlsx de acordo com csv passado por parametro e adiciona o dicionario
+def createXlsx(csv):
+    dicionary = [{'tag': tag, 'descricao': descricao}
+                 for tag, descricao in zip(dicionaryTags, dicionaryTypes)]
+
+    df_dict = pd.DataFrame(csv)
+    df_dicionary = pd.DataFrame(dicionary)
+
+    # Salve o DataFrame do dicionário em uma planilha específica dentro do arquivo CSV
+    with pd.ExcelWriter('analise_pobreza_brasil.xlsx', engine='openpyxl') as writer:
+        df_dict.to_excel(writer, sheet_name='Dados', index=False)
+        df_dicionary.to_excel(writer, sheet_name='Dicionario', index=False)
+
+    return
+
+
+# calcula a porcentagem de pessoas pobre, dividindo a quantidade de pessoas pobre/ total de pessoas
+def calculatePoorPercentage(csv):
+    percentage_pes_poor = []
+
+    for row in csv.iterrows():
+        poor_percentage = (row['qtd_pes_pobres'] / row['qtd_pes']) * 100
+        percentage_pes_poor.append(poor_percentage)
+
+    return percentage_pes_poor
+
+
+# calcula a porcentagem de pessoas vulneráveis, dividindo a quantidade de pessoas vulneráveis/ total de pessoas
+def calculateVulnerablePercentage(csv):
+    percentage_pes_vulnerable = []
+
+    for row in csv.iterrows():
+        vulnerable_percentage = (
+            row['qtd_pes_vulneraveis'] / row['qtd_pes']) * 100
+        percentage_pes_vulnerable.append(vulnerable_percentage)
+
+    return percentage_pes_vulnerable
+
+
+csv_data = pd.read_csv("MunicipioBrasil_20230102.csv")
+csv_data_filtered = csv_data[dicionaryTags]  # CSV a ser manipulado
+
+
+csv_data_filtered['percent_pes_pobres'] = calculatePoorPercentage()
+csv_data_filtered['percent_pes_vulneraveis'] = calculateVulnerablePercentage()
+
+# TODO falta calcular a media salarial por regiao.
+# TODO falta calcular a porcentagem de pessoas pobres, comparada ao total de pessoas por região.
+# TODO falta calcular a porcentagem de pessoas vulneráveis, comparada ao total de pessoas por região.
+
+createXlsx(csv_data_filtered)
