d8c293899267b638d479983a8eaf5749976687bb
henriqueFon <henriquearaujo091@gmail.com>
2023-10-26 11:23:16 -0300

    filtering avarage revenue for region

--- estudo-leitura-dos-dados/app.py
+++ estudo-leitura-dos-dados/app.py
@@ -67,15 +67,75 @@ def calculateVulnerablePercentage(csv):
 
     return percentage_pes_vulnerable
 
+#calcula a media de renda média para região norte
+def calculateAverageRevenueNorteRegion(csv):
+
+    database_filter_norte_region = csv[csv['cod_regiao'] == regionTags[0]]
+
+    avarage_revenue_norte_region = database_filter_norte_region['num_renda'].mean()
+
+    return avarage_revenue_norte_region
+
+#calcula a media de renda média para região nordeste
+def calculateAverageRevenueNordesteRegion(csv):
+
+    database_filter_nordeste_region = csv[csv['cod_regiao'] == regionTags[1]]
+
+    avarage_revenue_nordeste_region = database_filter_nordeste_region['num_renda'].mean()
+
+    return avarage_revenue_nordeste_region
+
+# calcula a media de renda média para região sudeste
+def calculateAverageRevenueSudesteRegion(csv):
+
+    database_filter_sudeste_region = csv[csv['cod_regiao'] == regionTags[2]]
+
+    avarage_revenue_sudeste_region = database_filter_sudeste_region['num_renda'].mean()
+
+    return avarage_revenue_sudeste_region
+
+# calcula a media de renda média para região sul
+def calculateAverageRevenueSulRegion(csv):
+
+    database_filter_sul_region = csv[csv['cod_regiao'] == regionTags[3]]
+
+    avarage_revenue_sul_region = database_filter_sul_region['num_renda'].mean()
+
+    return avarage_revenue_sul_region
+
+# calcula a media de renda média para região Centro Oeste
+def calculateAverageRevenueCentroOesteRegion(csv):
+
+    database_filter_centro_oeste_region = csv[csv['cod_regiao'] == regionTags[4]]
+
+    avarage_revenue_centro_oeste_region = database_filter_centro_oeste_region['num_renda'].mean()
+
+    return avarage_revenue_centro_oeste_region
+
+
+
+# Inicio das execuções
 
 csv_data = pd.read_csv("MunicipioBrasil_20230102.csv")
 csv_data_filtered = csv_data[dicionaryTags]  # CSV a ser manipulado
 
-print(csv_data_filtered)
-csv_data_filtered['percent_pes_pobres'] = calculatePoorPercentage(
-    csv_data_filtered)
+csv_data_filtered['percent_pes_pobres'] = calculatePoorPercentage(csv_data_filtered)
+
 csv_data_filtered['percent_pes_vulneraveis'] = calculateVulnerablePercentage(
     csv_data_filtered)
+
+csv_data_filtered['avarage_revenue_norte'] = calculateAverageRevenueNorteRegion(csv_data_filtered)
+
+csv_data_filtered['avarage_revenue_nordeste'] = calculateAverageRevenueNordesteRegion(csv_data_filtered)
+
+csv_data_filtered['avarage_revenue_sudeste'] = calculateAverageRevenueSudesteRegion(csv_data_filtered)
+
+csv_data_filtered['avarage_revenue_sul'] = calculateAverageRevenueSulRegion(csv_data_filtered)
+
+csv_data_filtered['avarage_revenue_centro_oeste'] = calculateAverageRevenueCentroOesteRegion(csv_data_filtered)
+
+print(csv_data_filtered)
+
 
 ############
 # TODO Criar uma nova aba para análise de dados por região
@@ -92,8 +152,6 @@ data_frame_regions_tags = [
     'percent_pes_vulneraveis',
 ]
 
-database_filter_norte_region = csv_data_filtered[csv_data_filtered['cod_regiao'] == regionTags[0]]
-print(database_filter_norte_region)
 
 
 # TODO falta calcular a media salarial por regiao.
